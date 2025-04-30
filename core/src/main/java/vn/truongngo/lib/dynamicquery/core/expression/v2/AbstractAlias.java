7f72e463ef16ed5ff8a54fd1be70accf5c438eb6
DESKTOP-1AVBG77\NieR <truong.ngo1993@gmail.com>
2025-04-30 23:42:07 +0700

    add

---
+++ core/src/main/java/vn/truongngo/lib/dynamicquery/core/expression/v2/AbstractAlias.java
@@ -0,0 +1,52 @@
+package vn.truongngo.lib.dynamicquery.core.expression.v2;
+
+import lombok.Getter;
+
+/**
+ * An abstract base class that provides a default implementation for aliasing behavior.
+ * <p>
+ * This class implements the {@link Aliasable} interface and allows expressions to be assigned an alias.
+ * It is commonly extended by concrete expression types such as columns, subqueries, or functions
+ * that support aliasing in query construction.
+ * </p>
+ * @param <T> the concrete type of the expression that supports aliasing
+ *
+ * @see Aliasable
+ * @see Expression
+ * @see Selection
+ * @see QuerySource
+ *
+ * @author Truong Ngo
+ * @version 2.0.0
+ */
+@Getter
+public abstract class AbstractAlias<T extends Expression> implements Aliasable<T>, Expression {
+
+    private String alias;
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getAlias() {
+        return alias;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    @SuppressWarnings("unchecked")
+    public T as(String alias) {
+        this.alias = alias;
+        return (T) this;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean hasAlias() {
+        return alias != null;
+    }
+}
