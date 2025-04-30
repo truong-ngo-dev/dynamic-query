8454f9e1cb4d8e91c7e9313d120502aa799b7cb8
DESKTOP-1AVBG77\NieR <truong.ngo1993@gmail.com>
2025-05-01 01:07:18 +0700

    add

---
+++ core/src/main/java/vn/truongngo/lib/dynamicquery/core/expression/v2/EntityReferenceExpression.java
@@ -0,0 +1,56 @@
+package vn.truongngo.lib.dynamicquery.core.expression.v2;
+
+import lombok.Getter;
+import vn.truongngo.lib.dynamicquery.core.builder.v2.Visitor;
+
+/**
+ * Represents an entity reference expression, typically used to refer to a specific entity class in a query.
+ * <p>
+ * This class encapsulates the reference to an entity class, allowing it to be used in queries to refer to
+ * the corresponding entity in a database. The class is part of a query source, providing the foundation
+ * for queries that interact with entities.
+ * </p>
+ * @author Truong Ngo
+ * @version 2.0.0
+ */
+@Getter
+public class EntityReferenceExpression extends AbstractAlias<EntityReferenceExpression> implements QuerySource {
+
+    private final Class<?> entityClass;
+
+    /**
+     * Constructs a new {@link EntityReferenceExpression} for the given entity class.
+     * <p>
+     * The entity class is used to reference a specific entity type in a query. This can be useful for building
+     * queries that refer to specific types of entities in an ORM context or custom query systems.
+     * </p>
+     *
+     * @param entityClass the class of the entity being referenced in the query
+     * @throws IllegalArgumentException if the provided entity class is {@code null}
+     */
+    public EntityReferenceExpression(Class<?> entityClass) {
+        if (entityClass == null) {
+            throw new IllegalArgumentException("Entity class must not be null");
+        }
+        this.entityClass = entityClass;
+    }
+
+    /**
+     * Accepts a visitor to allow processing or transformation of the entity reference expression.
+     * <p>
+     * This method is part of the visitor pattern, where different types of expressions are processed by a visitor
+     * that performs actions or transformations based on the expression type.
+     * </p>
+     *
+     * @param visitor the visitor that will process the entity reference expression
+     * @param context additional context to be passed to the visitor
+     * @param <R> the return type of the visitor's {@link Visitor#visit(EntityReferenceExpression, Object)} method
+     * @param <C> the type of the context passed to the visitor
+     * @return the result of the visitor's processing
+     */
+    @Override
+    public <R, C> R accept(Visitor<R, C> visitor, C context) {
+        return visitor.visit(this, context);
+    }
+
+}
