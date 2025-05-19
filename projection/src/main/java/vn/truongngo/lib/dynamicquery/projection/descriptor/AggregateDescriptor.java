e2b9c74cdfbd8cbaf0c3a927a66a2ddb0d389c58
DESKTOP-1AVBG77\NieR <truong.ngo1993@gmail.com>
2025-05-20 00:59:09 +0700

    add

---
+++ projection/src/main/java/vn/truongngo/lib/dynamicquery/projection/descriptor/AggregateDescriptor.java
@@ -0,0 +1,62 @@
+package vn.truongngo.lib.dynamicquery.projection.descriptor;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import vn.truongngo.lib.dynamicquery.core.enumerate.AggregateFunction;
+
+/**
+ * Descriptor representing an aggregate function selection in a query projection.
+ * <p>
+ * This class is typically constructed based on {@code @Aggregate} annotations,
+ * and is used to describe aggregate expressions such as {@code SUM}, {@code COUNT}, etc.,
+ * to be included in the SELECT clause.
+ * </p>
+ *
+ * <h2>Example</h2>
+ * <blockquote><pre>
+ * {@code @Aggregate}(function = "SUM", column = "amount", alias = "totalAmount")
+ * private BigDecimal totalAmount;
+ * </pre></blockquote>
+ *
+ * <p>
+ * The {@code selection} represents the target column or expression being aggregated.
+ * The {@code alias} defines how the result should be referenced in the result set.
+ * </p>
+ *
+ * @author Truong Ngo
+ * @version 2.0.0
+ */
+@Getter
+@Setter
+@Builder
+public class AggregateDescriptor implements SelectDescriptor {
+
+    /**
+     * The aggregate function to apply (e.g., SUM, COUNT, AVG).
+     */
+    private AggregateFunction function;
+
+    /**
+     * The descriptor representing the column or expression to be aggregated.
+     */
+    private SelectDescriptor selection;
+
+    /**
+     * The alias used to reference the result of the aggregate function in the projection.
+     */
+    private String alias;
+
+    /**
+     * Indicates whether the aggregation should apply only to distinct values.
+     */
+    private boolean distinct;
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getAlias() {
+        return alias;
+    }
+}
